0748bc8d5d23a3c5903e9651116b1c2e14115877
Deep Narayan Mishra <atdeepmishra@gmail.com>
2018-05-25 15:48:47 -0400

    Updated program with comments

--- UniqueCharacters.java
+++ UniqueCharacters.java
@@ -7,19 +7,24 @@
 
 public class UniqueCharacters {
 	
+	/**
+	 * Method returns true if the string contains all unique characters.
+	 * 
+	 * The complexity of program is O(n) with extra space for holding boolean array of charsets
+	 * 
+	 * @param str
+	 * @return
+	 */
 	public static boolean hasUniqueChars(String str) {
-		if(str.length() > 256) return false;
-		
+		if(str.length() > 256) return false;		
 		boolean[] charSet = new boolean[Character.MAX_VALUE];
 		for(int i=0; i<str.length(); i++) {
 			int utf16 = str.charAt(i);
 			if(charSet[utf16]) {
 				return false;
 			}
-			
 			charSet[utf16] = true;
-		}
-		
+		}		
 		return true;
 	}
 	
@@ -27,9 +32,7 @@ public class UniqueCharacters {
 		
 		System.out.println("Input: " + "abcded" + " ---- Output: " + hasUniqueChars("abcded"));
 		System.out.println("Input: " + "abcdewl" + " ---- Output: " + hasUniqueChars("abcdewl"));
-	
 	}
-
 }
 
 /**
