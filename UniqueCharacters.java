f644a8c1b7d32ba17c96a40f0fdb2d6377d27f1c
Deep Narayan Mishra <atdeepmishra@gmail.com>
2018-05-25 15:57:06 -0400

    Better algorithm

--- UniqueCharacters.java
+++ UniqueCharacters.java
@@ -8,15 +8,13 @@
 public class UniqueCharacters {
 	
 	/**
-	 * Method returns true if the string contains all unique characters.
-	 * 
-	 * The complexity of program is O(n) with extra space for holding boolean array of charsets
-	 * 
+	 * Method returns true if the string contains all unique characters.	 * 
+	 * The complexity of program is O(n) with extra space for holding boolean array of charsets	 * 
 	 * @param str
 	 * @return
 	 */
 	public static boolean hasUniqueChars(String str) {
-		if(str.length() > 256) return false;		
+		if(str.length() > Character.MAX_VALUE) return false;		
 		boolean[] charSet = new boolean[Character.MAX_VALUE];
 		for(int i=0; i<str.length(); i++) {
 			int utf16 = str.charAt(i);
@@ -28,16 +26,43 @@ public class UniqueCharacters {
 		return true;
 	}
 	
+	/**
+	 * Method returns true if the string contains all unique characters.
+	 * The complexity of program is O(n) with LESS extra space
+	 * @param str
+	 * @return
+	 */
+	public static boolean hasUniqueCharsBetter(String str) {
+		if(str.length() > Character.MAX_VALUE) return false;		
+		int checker = 0;
+		for(int i=0; i<str.length(); i++) {
+			int value = str.charAt(i) - 'a';
+			if((checker & (1 << value)) > 0) {
+				return false;
+			}
+			checker |= (1 << value);
+		}		
+		return true;
+	}
+	
 	public static void main(String[] args) {
-		
+		System.out.println("Technique 1:");
 		System.out.println("Input: " + "abcded" + " ---- Output: " + hasUniqueChars("abcded"));
 		System.out.println("Input: " + "abcdewl" + " ---- Output: " + hasUniqueChars("abcdewl"));
+		
+		System.out.println("Technique 2:");
+		System.out.println("Input: " + "abcded" + " ---- Output: " + hasUniqueCharsBetter("abcded"));
+		System.out.println("Input: " + "abcdewl" + " ---- Output: " + hasUniqueCharsBetter("abcdewl"));
 	}
 }
 
 /**
  * Outputs: 
 
+Technique 1:
+Input: abcded ---- Output: false
+Input: abcdewl ---- Output: true
+Technique 2:
 Input: abcded ---- Output: false
 Input: abcdewl ---- Output: true
 */
