afe35bf726cd0ef604d9b4fa7e6d44d92aa76cc6
Deep Narayan Mishra <atdeepmishra@gmail.com>
2018-05-27 18:00:19 -0400

    Updated outputs

--- MatrixRotation.java
+++ MatrixRotation.java
@@ -58,3 +58,33 @@ public class MatrixRotation {
 		display(mat2);
 	}
 }
+
+/**
+ * Output:
+CASE1:
+Input Matrix
+1	2	3	4	
+5	6	7	8	
+9	10	11	12	
+13	14	15	16	
+Rotated Matrix
+13	9	5	1	
+14	10	6	2	
+15	11	7	3	
+16	12	8	4	
+
+CASE2:
+Input Matrix
+1	2	3	4	5	
+6	7	8	9	10	
+11	12	13	14	15	
+16	17	18	19	20	
+21	22	23	24	25	
+Rotated Matrix
+21	16	11	6	1	
+22	17	12	7	2	
+23	18	13	8	3	
+24	19	14	9	4	
+25	20	15	10	5	
+
+*/
