dc4d527afa1b80bb5b3deedace97ab306df15fcb
Deep Narayan Mishra <atdeepmishra@gmail.com>
2018-05-28 01:11:49 -0400

    Added program for removing duplicates

---
+++ RemoveDuplicateLL.java
@@ -0,0 +1,76 @@
+import java.util.Hashtable;
+
+/**
+ * Remove duplicate from unsorted linked list
+ * @author Deep
+ *
+ */
+
+class LinkedListNode {
+	int data;
+	LinkedListNode next;
+	LinkedListNode(int d) {data=d;}
+}
+
+
+public class RemoveDuplicateLL {
+	
+	public static void removeDuplicate(LinkedListNode node) {
+		
+		Hashtable<Integer, Boolean> htable = new Hashtable<Integer, Boolean>();
+		LinkedListNode prev = null;
+		while(node != null) {
+			if(htable.containsKey(node.data)) {
+				prev.next = node.next;
+			} else {
+				htable.put(node.data,true);
+				prev = node;
+			}
+			node = node.next;
+		}
+			
+	}
+	
+	/**Display the List
+	 * 
+	 * @param node
+	 */
+	public static void show(LinkedListNode node) {
+		while(node != null) {
+			System.out.print( node.data + " -> ");
+			node = node.next;
+		}
+		System.out.print("null");
+	}
+	
+	public static void main(String[] args) {
+		
+		LinkedListNode head, n1;
+		n1 = new LinkedListNode(2);
+		head = n1;
+		n1.next = new LinkedListNode(4);
+		n1 = n1.next;
+		n1.next = new LinkedListNode(3);
+		n1 = n1.next;
+		n1.next = new LinkedListNode(4);
+		n1 = n1.next;
+		n1.next = new LinkedListNode(2);
+		n1 = n1.next;
+		System.out.println("Input List");
+		show(head);
+		removeDuplicate(head);
+		System.out.println();
+		System.out.println("Final List");
+		show(head);
+	}
+
+}
+
+/**
+ * Output :
+Input List
+2 -> 4 -> 3 -> 4 -> 2 -> null
+Final List
+2 -> 4 -> 3 -> null
+*
+*/
